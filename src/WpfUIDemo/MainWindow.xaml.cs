54c286c321c5e5f1f33069f3d384a9e939fd417c
walterlv <xblero@gmail.com>
2017-10-02 17:00:00 +0800

    Add demo project.

---
+++ src/WpfUIDemo/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Walterlv.Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleHeader_MouseMove(object sender, MouseEventArgs e)
+        {
+            // 鼠标按下的时候，可以拖拽窗口。
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
